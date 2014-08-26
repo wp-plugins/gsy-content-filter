463d8b282f55612679f326f0fe7445df07d8ad50
georgi.s.yankov <georgi.s.yankov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-26 17:35:14 +0000

    Initial Upload
    git-svn-id: https://plugins.svn.wordpress.org/gsy-content-filter/branches/1.0@973461 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gsy-content-filter.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Plugin Name: GSY Content Filter
+ * Plugin URI: https://github.com/georgi-yankov/gsy-content-filter
+ * Description: Temporarily or permanently filter key words from the title, content, excerpt or tags in posts.
+ * Version: 1.0
+ * Author: Georgi Yankov
+ * Author URI: http://gsy-design.com
+ * Text Domain: gsy-content-filter
+ * License: GPLv2
+ */
+
+/* Copyright 2014 Georgi Yankov (email : georgi.st.yankov@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+require_once 'includes/class-gsy-content-filter.php';
+
+$gsy_content_filter = new GSY_Content_Filter();
